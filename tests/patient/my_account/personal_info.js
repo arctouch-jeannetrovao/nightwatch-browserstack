e271267eb77f9c14b3e6948a01fd830da76b748a
Jeanne Trovão <jeanne.trovao@arctouch.com>
2020-10-09 10:55:59 -0300

    workaround of Address 1 field

--- tests/patient/my_account/personal_info.js
+++ tests/patient/my_account/personal_info.js
@@ -9,12 +9,12 @@ module.exports = {
 
         personalInfoPage.accessPersonalInfoPage()
             //edit fields in Personal Info form according to parameters values
-            .editPersonalInfo("First Name Edited", "Middle Name Edited","Last Name Edited", "Address 1 Edited",
+            .editPersonalInfo("First Name Edited", "Middle Name Edited","Last Name Edited", "Address1Edited",
             "Address 2 Edited", "City Edited", "WyomingOption", "12345", "123-456-7891", "09/06/1990", "femaleOption", "AmericaNoronha020200Option")
             //check Success toast
             .checkToastMessage("Personal Info updated.")
             //check if all fields were saved as informed previously
-            .checkPersistence("First Name Edited", "Middle Name Edited","Last Name Edited", "Address 1 Edited",
+            .checkPersistence("First Name Edited", "Middle Name Edited","Last Name Edited", "Address1Edited",
             "Address 2 Edited", "City Edited", "Wyoming", "12345", "123-456-7891", "09/06/1990", "Female", "America/Noronha (-02 -0200)")
     },
 
