32836a5972835684c3ddf62534b0ec688dbd2737
Jeanne Trovão <jeanne.trovao@arctouch.com>
2020-10-08 23:51:58 -0300

    saucelab configuration

--- commands/editTextField.js
+++
@@ -1,6 +0,0 @@
-exports.command = function editTextField(locator, value) {
-    return this
-        .waitForElementVisible(locator)
-        .clearValue2(locator)
-        .setValue(locator, value)
-  }
